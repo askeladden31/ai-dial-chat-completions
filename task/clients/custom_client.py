3e6d1c075165cf06118d3da185257cd01ced4dd1
TravelMate <you@example.com>
2025-12-30 19:06:09 +0200

    Work in progress

--- task/clients/custom_client.py
+++ task/clients/custom_client.py
@@ -8,7 +8,7 @@ from task.models.message import Message
 from task.models.role import Role
 
 
-class DialClient:
+class DialClient(BaseClient):
     _endpoint: str
     _api_key: str
 
@@ -28,7 +28,24 @@ class DialClient:
         #   - json: request_data from step 2
         # 4. Get content from response, print it and return message with assistant role and content
         # 5. If status code != 200 then raise Exception with format: f"HTTP {response.status_code}: {response.text}"
-        raise NotImplementedError
+
+        headers = {
+            "api-key": self._api_key,
+            "Content-Type": "application/json"
+        }
+
+        request_data = {
+            "messages": [msg.to_dict() for msg in messages]
+        }
+
+        response = requests.post(self._endpoint, headers=headers, json=request_data)
+
+        if response.status_code != 200:
+            raise Exception(f"HTTP {response.status_code}: {response.text}")
+
+        content = response.content
+        print(content)
+        return Message(role=Role.AI, content=content)
 
     async def stream_completion(self, messages: list[Message]) -> Message:
         #TODO:
@@ -46,5 +63,30 @@ class DialClient:
         #    - Use 'async with' context manager for response
         # 6. Get content from chunks (don't forget that chunk start with `data: `, final chunk is `data: [DONE]`), print
         #    chunks, collect them and return as assistant message
+
+        headers = {
+            "api-key": self._api_key,
+            "Content-Type": "application/json"
+        }
+
+        request_data = {
+            "stream": True,
+            "messages": [msg.to_dict() for msg in messages]
+        }
+
+        contents = []
+
+        async with aiohttp.ClientSession() as session:
+            async with session.post(url=self._endpoint, headers=headers, json=request_data) as response:
+                async for chunk in response.content:
+                    line = chunk.decode('utf-8').strip()
+                    payload = line[6:].strip()
+                    if payload != "[DONE]":
+                        self._g
+
         raise NotImplementedError
 
+    def _get_content_snippet(self, data: str):
+        json_data = json.loads(data)
+        if choices := json_data.choices:
+            delta = choices[0]["delta"]
